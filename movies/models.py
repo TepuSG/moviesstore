de31ed7b82d0124dbfd3d844ee247fcb3c64e8ba
Pedro <pedro@Pedros-MacBook-Pro-4.local>
2025-10-19 15:56:33 -0400

    Added Rating model and database migrations

--- movies/models.py
+++ movies/models.py
@@ -6,8 +6,20 @@ class Movie(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/')
+
     def __str__(self):
         return str(self.id) + ' - ' + self.name
+
+    def average_rating(self):
+        """Calculate the average rating for this movie"""
+        ratings = self.rating_set.all()
+        if ratings:
+            return sum(rating.rating for rating in ratings) / len(ratings)
+        return 0
+
+    def rating_count(self):
+        """Get total number of ratings for this movie"""
+        return self.rating_set.count()
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
     comment = models.CharField(max_length=255)
@@ -20,4 +32,20 @@ class Review(models.Model):
     likes = models.ManyToManyField(User, related_name='liked_reviews', blank=True)
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # 1-5 stars
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')  # One rating per user per movie
+
+    def __str__(self):
+        return f'{self.user.username} - {self.movie.name} - {self.rating} stars'
+
+
 # Create your models here.
